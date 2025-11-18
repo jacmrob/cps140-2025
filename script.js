7e54cd76c51aed8be42a1cf1970f61111f518663
jacmrob <jacqueline.roberti93@gmail.com>
2025-11-18 09:58:56 -0500

    navbar changes

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello!');
+// load the nav bar onto the page
+fetch('/navbar.html') // fetch from file
+    .then(response => response.text())
+    .then(data => {
+      // load the nav bar into our div with id 'navbar'
+      document.getElementById('navbar').innerHTML = data;
+
+      const links = document.querySelectorAll('#navbar nav a');
+      links.forEach(link => {
+      if (link.href === window.location.href) {
+          link.setAttribute('aria-current', 'page');
+      }
+    });
+
+  });
+
